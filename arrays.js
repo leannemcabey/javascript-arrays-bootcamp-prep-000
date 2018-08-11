bf3c8cb9b3e2355b43d599c7cb47e4477cc2be0a
Leanne Cabey <leanne.m.cabey@gmail.com>
2018-08-11 22:51:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
-  return list
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
