78531914f36b458e5d7ffe44b920ab11770b10d8
Leanne Cabey <leanne.m.cabey@gmail.com>
2018-08-11 22:02:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(list, element) {
-  
+  return [element, ...list]
 }
 
 function destructivelyAddElementToBeginningOfArray(list, element) {
-  
+  return list.unshift(element)
 }
+
+function 
